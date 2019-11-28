c7751f880a303112d0bb8de6069b2c828c33387f
Sashi Kumar Ram <sashi.suraj7@gmail.com>
2019-11-28 08:52:57 +0530

    hnavbar commit

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,16 +1,56 @@
-var authorcard = document.querySelector('.author-cards');
-var cardbody = document.querySelector('.card-body');
-var plusicon = document.querySelector('.plus-icon');
+// var authorcard = document.querySelectorAll('.author-cards');
 
-authorcard.addEventListener('mouseenter', function(event) {
-    console.log(event);
-    cardbody.classList.add('card-body-hover');
-    plusicon.classList.remove('d-none');
-});
 
-authorcard.addEventListener('mouseleave', function(event) {
-    console.log(event);
-    cardbody.classList.remove('card-body-hover');
-    plusicon.classList.add('d-none');
-});
+// for(let i = 0; i < authorcard.length; i++) {
+//     authorcard[i].addEventListener("mouseenter", function(event) {
+//         event.target.classList.add('card-body-hover');
+//         event.target.querySelector('.plus-icon').classList.remove('d-none');
+//     }) 
+//   }
+
+//   for(let i = 0; i < authorcard.length; i++) {
+//     authorcard[i].addEventListener("mouseleave", function(event) {
+//         event.target.classList.remove('card-body-hover');
+//         event.target.querySelector('.plus-icon').classList.add('d-none');
+//     }) 
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// authorcardArray.forEach(elem => {
+//     console.log(elem);
+//     elem.addEventListener('mouseenter', addOverlay(event));
+// })
+
+// authorcardArray.forEach( elem => {
+//     console.log(elem);
+//     elem.addEventListener('mouseleave', removeOverlay(event));
+// })
+
+// function addOverlay(e) {
+   
+//     e.target.classList.add('card-body-hover');
+//     e.target.querySelector('.plus-icon').classList.remove('d-none');
+// };
+
+// function removeOverlay(event) {
+//     console.log(event);
+//     event.target.classList.remove('card-body-hover');
+//     event.target.querySelector('.plus-icon').classList.add('d-none');
+
+// authorcard.addEventListener('mouseleave', function(event) {
+//     console.log(event);
+//     event.target.classList.remove('card-body-hover');
+//     event.target.querySelector('.plus-icon').classList.add('d-none');
+// });
 
